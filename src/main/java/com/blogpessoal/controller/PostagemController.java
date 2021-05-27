71b4b0000e7c2f8c3354e78d33c9b20fe67e3326
fabriciio95 <fabriciusiqueira@gmail.com>
2021-05-27 11:05:56 -0300

    Crud de Postagem

--- src/main/java/com/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/blogpessoal/controller/PostagemController.java
@@ -2,11 +2,19 @@ package com.blogpessoal.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blogpessoal.model.Postagem;
@@ -35,6 +43,33 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> findByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(postagemRepository.findByTituloContainingIgnoreCase(titulo));
 	}
+	
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Postagem novaPostagem(@RequestBody @Valid Postagem postagem) {
+		return postagemRepository.save(postagem);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Postagem> atualizar(@PathVariable Long id, @RequestBody @Valid Postagem postagem) {
+		if(!postagemRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(postagemRepository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		if(!postagemRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		postagemRepository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
 	
 	
 }
